5d41a5bafaddec2ac2cadd9665bda63589d81048
proflealvjo <profleonardo.barros@fiap.com.br>
2024-09-05 17:33:26 -0300

    feat: Usando contrato nos testes

--- src/test/java/services/CadastroEntregasService.java
+++ src/test/java/services/CadastroEntregasService.java
@@ -1,10 +1,24 @@
 package services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import model.EntregaModel;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
 
 import static io.restassured.RestAssured.given;
 
@@ -17,6 +31,9 @@ public class CadastroEntregasService {
     public Response response;
     String baseUrl = "http://localhost:8080";
     String idDelivery;
+    String schemasPath = "src/test/resources/schemas/";
+    JSONObject jsonSchema;
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public void setFieldsDelivery(String field, String value) {
         switch (field) {
@@ -55,5 +72,38 @@ public class CadastroEntregasService {
                 .then()
                 .extract()
                 .response();
+    }
+
+    private JSONObject loadJsonFromFile(String filePath) throws IOException {
+        try (InputStream inputStream = Files.newInputStream(Paths.get(filePath))) {
+            JSONTokener tokener = new JSONTokener(inputStream);
+            return new JSONObject(tokener);
+        }
+    }
+
+    public void setContract(String contract) throws IOException {
+        switch (contract) {
+            case "Cadastro bem-sucedido de entrega" -> jsonSchema = loadJsonFromFile(schemasPath + "cadastro-bem-sucedido-de-entrega.json");
+            default -> throw new IllegalStateException("Unexpected contract" + contract);
+        }
+    }
+
+    public Set<ValidationMessage> validateResponseAgainstSchema() throws IOException {
+
+        // Obter o corpo da resposta como String e converter para JSONObject
+        JSONObject jsonResponse = new JSONObject(response.getBody().asString());
+
+        // Configurar o JsonSchemaFactory e criar o JsonSchema
+        JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4);
+        JsonSchema schema = schemaFactory.getSchema(jsonSchema.toString());
+
+        // Converter o JSON de resposta para JsonNode
+        JsonNode jsonResponseNode = mapper.readTree(jsonResponse.toString());
+
+        // Validar o JSON de resposta contra o esquema
+        Set<ValidationMessage> schemaValidationErrors = schema.validate(jsonResponseNode);
+
+        return schemaValidationErrors;
+
     }
 }

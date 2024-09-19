ba4db4e10d5f2dc902a37e1d916239156c3a61dd
proflealvjo <profleonardo.barros@fiap.com.br>
2024-09-19 17:16:26 -0300

    feat: incluindo novo test de e2e

--- src/test/java/steps/CadastroEntregasSteps.java
+++ src/test/java/steps/CadastroEntregasSteps.java
@@ -20,47 +20,46 @@ public class CadastroEntregasSteps {
 
     @Dado("que eu tenha os seguintes dados da entrega:")
     public void queEuTenhaOsSeguintesDadosDaEntrega(List<Map<String, String>> rows) {
-       // for (Map<String, String> columns : rows) {
-          //  cadastroEntregasService.setFieldsDelivery(columns.get("campo"), columns.get("valor"));
-        //}
+        for (Map<String, String> columns : rows) {
+            cadastroEntregasService.setFieldsDelivery(columns.get("campo"), columns.get("valor"));
+        }
     }
 
     @Quando("eu enviar a requisição para o endpoint {string} de cadastro de entregas")
     public void euEnviarARequisiçãoParaOEndpointDeCadastroDeEntregas(String endPoint) {
-        //adastroEntregasService.createDelivery(endPoint);
+        cadastroEntregasService.createDelivery(endPoint);
     }
 
     @Então("o status code da resposta deve ser {int}")
     public void oStatusDaRespostaDeveSer(int statusCode) {
-        //Assert.assertEquals(statusCode, cadastroEntregasService.response.statusCode());
+        Assert.assertEquals(statusCode, cadastroEntregasService.response.statusCode());
     }
 
     @E("o corpo de resposta de erro da api deve retornar a mensagem {string}")
     public void oCorpoDeRespostaDeErroDaApiDeveRetornarAMensagem(String message) {
-        //ErrorMessageModel errorMessageModel = cadastroEntregasService.gson.fromJson(
-           //     cadastroEntregasService.response.jsonPath().prettify(), ErrorMessageModel.class);
-        //Assert.assertEquals(message, errorMessageModel.getMessage());
+        ErrorMessageModel errorMessageModel = cadastroEntregasService.gson.fromJson(
+                cadastroEntregasService.response.jsonPath().prettify(), ErrorMessageModel.class);
+        Assert.assertEquals(message, errorMessageModel.getMessage());
     }
 
     @Dado("que eu recupere o ID da entrega criada no contexto")
     public void queEuRecupereOIDDaEntregaCriadaNoContexto() {
-        //cadastroEntregasService.retrieveIdDelivery();
+        cadastroEntregasService.retrieveIdDelivery();
     }
 
     @Quando("eu enviar a requisição com o ID para o endpoint {string} de deleção de entrega")
     public void euEnviarARequisiçãoComOIDParaOEndpointDeDeleçãoDeEntrega(String endPoint) {
-        //cadastroEntregasService.deleteDelivery(endPoint);
+        cadastroEntregasService.deleteDelivery(endPoint);
     }
 
     @E("que o arquivo de contrato esperado é o {string}")
     public void queOArquivoDeContratoEsperadoÉO(String contract) throws IOException {
-        //cadastroEntregasService.setContract(contract);
+        cadastroEntregasService.setContract(contract);
     }
 
     @Então("a resposta da requisição deve estar em conformidade com o contrato selecionado")
     public void aRespostaDaRequisiçãoDeveEstarEmConformidadeComOContratoSelecionado() throws IOException {
-        //Set<ValidationMessage> validateResponse = cadastroEntregasService.validateResponseAgainstSchema();
-        //Assert.assertTrue("O contrato está inválido. Erros encontrados: " + validateResponse, validateResponse.isEmpty());
-
+        Set<ValidationMessage> validateResponse = cadastroEntregasService.validateResponseAgainstSchema();
+        Assert.assertTrue("O contrato está inválido. Erros encontrados: " + validateResponse, validateResponse.isEmpty());
     }
 }

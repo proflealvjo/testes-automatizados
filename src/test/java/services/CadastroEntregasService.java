b5f9b5f675101f90e821ade57c23f496b8edaf34
proflealvjo <profleonardo.barros@fiap.com.br>
2025-04-29 14:19:25 -0300

    feat: nova url para testes

--- src/test/java/services/CadastroEntregasService.java
+++ src/test/java/services/CadastroEntregasService.java
@@ -29,7 +29,7 @@ public class CadastroEntregasService {
             .excludeFieldsWithoutExposeAnnotation()
             .create();
     public Response response;
-    String baseUrl = "https://api-entregas-e08b1747db7d.herokuapp.com";
+    String baseUrl = "https://tds-api-entregas-25cc1f0ca34b.herokuapp.com";
     String idDelivery;
     String schemasPath = "src/test/resources/schemas/";
     JSONObject jsonSchema;
